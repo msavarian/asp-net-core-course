855ff65a17c15a9379c5b4f870b7de85181c26e8
msavarian <sav68.net@gmail.com>
2023-07-25 01:52:41 +0330

    init

---
+++ Session10.MvcAndRazorPageDemo/S10.AspNetCore.MvcDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S10.AspNetCore.MvcDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
